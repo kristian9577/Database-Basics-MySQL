8d3d1b2490bdb56766daab7ea6ba8d230d9b12ad
kristian9577 <kristian9577@gmail.com>
2019-05-27 22:27:34 +0300

    add files

---
+++ 05.Built-in Functions - Lab/built_in_lab.sql
@@ -0,0 +1,23 @@
+--    1.Find Book Titles
+SELECT title FROM books 
+WHERE SUBSTRING(title,1,3) = "The"
+ORDER BY `id`;
+
+--  2.Replace Titles
+SELECT REPLACE (`title`,'The','***')
+AS 'Title' FROM `books`
+WHERE SUBSTRING(title,1,3) = "The"
+ORDER BY `id`;
+
+-- 3.Sum Cost of All Books
+SELECT SUM(`cost`)
+FROM books;
+
+-- 4.Days Lived
+SELECT CONCAT(`first_name`,' ',`last_name`) AS 'Full Name',
+TIMESTAMPDIFF(DAY,`born`,`died`) AS `Days Lived` FROM `authors`;
+
+-- 5.Harry Potter Books
+SELECT title FROM books 
+WHERE SUBSTRING(title,1,12) = "Harry Potter"
+ORDER BY `id`;
